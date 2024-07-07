6aca88bcf981c7802628915b04fb8d8efcdb421c
lanroo <yladacs@gmail.com>
2024-07-07 19:06:33 -0300

    initials

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+import logoimg from "./images/Logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoimg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o Mundo</h1>
+          <p>Visite orfanatos e mude a vida de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Belém</strong>
+          <span>PA</span>
+        </div>
+
+        <a href="" className="enter-app"></a>
+      </div>
     </div>
   );
 }
