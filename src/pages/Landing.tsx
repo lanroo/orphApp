e8661acdd475929a55621d21ff8827b2f5e53076
lanroo <yladacs@gmail.com>
2024-07-07 20:21:41 -0300

    page map

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/pages/landing.css";
 import logoimg from "../images/Logo.svg";
 import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
@@ -19,9 +20,9 @@ function Landing() {
           <span>PA</span>
         </div>
 
-        <a href="/app" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
+        </Link>
       </div>
     </div>
   );
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import mapMarkerImg from "../images/map-marker.svg";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
 
 function OrphanagesMap() {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2> Escolha um orfanato no Mapa </h2>
+          <p>Muitas crianças esperam a sua visita ;)</p>
+        </header>
+
+        <footer>
+          <strong>Belém</strong>
+          <span>Pará</span>
+        </footer>
+      </aside>
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
