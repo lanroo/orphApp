b023faed013ee386a20a14f1621be5cd12e7395c
lanroo <yladacs@gmail.com>
2024-07-07 21:48:12 -0300

    map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,30 +1,63 @@
-import React from "react";
-import mapMarkerImg from "../images/map-marker.svg";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-function OrphanagesMap() {
+import mapMarkerImg from "../images/map-marker.svg";
+import "../styles/pages/orphanages-map.css";
+
+// Configuração do ícone padrão do Leaflet
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+// Componente para centralizar o mapa
+const FlyMapTo: React.FC<{ center: [number, number] }> = ({ center }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    map.flyTo(center);
+  }, [center, map]);
+
+  return null;
+};
+
+const OrphanagesMap: React.FC = () => {
+  const center: [number, number] = [-1.4373104, -48.468732];
+
   return (
     <div id="page-map">
       <aside>
         <header>
           <img src={mapMarkerImg} alt="Happy" />
-          <h2> Escolha um orfanato no Mapa </h2>
+          <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças esperam a sua visita ;)</p>
         </header>
-
         <footer>
           <strong>Belém</strong>
           <span>Pará</span>
         </footer>
       </aside>
-      <div></div>
+
+      <MapContainer
+        center={center}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+        scrollWheelZoom={true}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <FlyMapTo center={center} />
+      </MapContainer>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
   );
-}
+};
 
 export default OrphanagesMap;
